cce3dfdcc9e8b36a64408d492ff258ee22187123
Paul Petrov <paul@bargamut.ru>
2020-07-01 20:38:37 +0300

    Create example for manageable components in forms

---
+++ src/09-forms-manageable-components.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class NameForm extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { value: `` };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({
+			value: event.target.value,
+		});
+	}
+
+	handleSubmit(event) {
+		alert(`Отправленное имя: ${this.state.value}`);
+
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<label>
+					Имя:
+					<input value={this.state.value} onChange={this.handleChange} />
+				</label>
+
+				<input type="submit" value="Отправить" />
+			</form>
+		);
+	}
+}
+
+ReactDOM.render(
+	<NameForm />,
+	document.getElementById(`forms-manageable-components`)
+);
