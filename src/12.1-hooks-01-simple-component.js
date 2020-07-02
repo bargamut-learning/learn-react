1b7d247012e12b436ce6210d5d2b778eb9bcb317
Paul Petrov <paul@bargamut.ru>
2020-07-02 13:20:29 +0300

    Hooks: create a simple example & update style.css

---
+++ src/12.1-hooks-01-simple-component.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { useState } = React;
+
+function Example() {
+	// Объявление переменной состояния "count" со значением "0"
+	const [ count, setCount ] = useState(0);
+
+	return (
+		<div>
+			<p>Вы кликнули {count} раз</p>
+
+			<button onClick={() => setCount(count + 1)}>
+				Нажми на меня
+			</button>
+		</div>
+	);
+}
+
+ReactDOM.render(
+	<Example />,
+	document.getElementById(`hooks-01-simple-component`)
+);
