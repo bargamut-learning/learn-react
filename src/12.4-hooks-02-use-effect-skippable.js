e0da5d2b50d25f886acafa084ace8d66335dee42
Paul Petrov <paul@bargamut.ru>
2020-07-06 18:06:58 +0300

    HOOKS: create example for skippable useEffect

---
+++ src/12.4-hooks-02-use-effect-skippable.js
@@ -0,0 +1,59 @@
+'use strict';
+
+(() => {
+	// Mock-данные
+	const friendList = [
+		{ id: 0, name: `Paul`, isOnline: () => (Math.random() > .5) },
+		{ id: 1, name: `Natalia`, isOnline: () => (Math.random() > .5) },
+	];
+	const ChatAPI = {
+		subscribeToFriendStatus: (id, handler) => {
+			handler({ isOnline: friendList[id].isOnline() });
+
+			return true;
+		},
+		unsubscribeFromFriendStatus: (id, handler) => {
+			handler({ isOnline: friendList[id].isOnline() });
+
+			return false;
+		}
+	};
+
+	const { useState, useEffect } = React;
+
+	function FriendStatus(props) {
+		const [ isOnline, setIsOnline ] = useState(null);
+
+		useEffect(() => {
+			function handleStatusChange(status) {
+				setIsOnline(status.isOnline);
+			}
+
+			ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+
+			console.log(`${props.friend.id} useEffect skippable start`);
+
+			return function cleanup() {
+				ChatAPI.unsubscribeFromFriendStatus(props.friend.id, handleStatusChange);
+				
+				console.log(`${props.friend.id} useEffect skippable cleanup`);
+			};
+		}, [ props.friend.id ]); // Повторно подписаться, только если props.friend.id изменился
+
+		if (isOnline === null) return `Загрузка...`;
+
+		return (
+			<div>
+				{props.friend.name}: {isOnline ? `В сети` : `Не в сети`}
+			</div>
+		);
+	}
+
+	ReactDOM.render(
+		<>
+			{/* <FriendStatus friend={friendList[0]} /> */}
+			{friendList.map((friend) => <FriendStatus key={friend.id} friend={friend} />)}
+		</>,
+		document.getElementById(`hooks-02-use-effect-skippable`)
+	);
+})();
